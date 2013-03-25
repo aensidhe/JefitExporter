202f5ec58a48cdd092c9bb84ed173f540c869b32
aensidhe <me@aensidhe.ru>
2013-03-25 19:58:02 +0400

    Ready for integrating of JodaTIme.

--- src/ru/AenSidhe/jefit/exporter/xlsx/JeFitExport.java
+++ src/ru/AenSidhe/jefit/exporter/xlsx/JeFitExport.java
@@ -2,6 +2,9 @@ package ru.AenSidhe.jefit.exporter.xlsx;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.*;
+import android.app.*;
+import java.util.*;
 
 public class JeFitExport extends Activity
 {
@@ -9,5 +12,29 @@ public class JeFitExport extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.jefit_export);
+	}
+	
+	public void exportButtonClick(View view)
+	{
+		Date startDate = getStartDateEditor().getDate();
+		Date endDate = getEndDateEditor().getDate();
+		new AlertDialog.Builder(this)
+			.setMessage(
+				(startDate != null ? startDate.toGMTString() : "<null>") + 
+				"\n" + 
+				(endDate != null ? endDate.toGMTString() : "<null>")
+			)
+			.create()
+			.show();
+	}
+	
+	private DateTimeFragment getStartDateEditor()
+	{
+		return (DateTimeFragment) getFragmentManager().findFragmentById(R.id.jefit_export_startDate);
+	}
+	
+	private DateTimeFragment getEndDateEditor()
+	{
+		return (DateTimeFragment) getFragmentManager().findFragmentById(R.id.jefit_export_endDate);
 	}
 }
