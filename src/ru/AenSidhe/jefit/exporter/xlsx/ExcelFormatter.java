5a1a8674952d5ad69a04eb638b2c87e00bdefc53
aensidhe <me@aensidhe.ru>
2013-03-29 12:02:37 +0400

    No xlsx for Android, unfortunately. Only *.xls now.

--- src/ru/AenSidhe/jefit/exporter/xlsx/ExcelFormatter.java
+++ src/ru/AenSidhe/jefit/exporter/xlsx/ExcelFormatter.java
@@ -1,18 +1,19 @@
 package ru.AenSidhe.jefit.exporter.xlsx;
+
 import java.io.*;
 import java.util.*;
 
 import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.*;
+import org.apache.poi.hssf.usermodel.*;
 import org.joda.time.*;
 import android.content.*;
 
 public class ExcelFormatter
 {
-	public String CreateExcel(Iterator<Set> sets, Context context) throws FileNotFoundException, IOException
+	public String CreateExcel(Iterator<Set> sets, Context context) throws IOException
 	{
-		Workbook wb = new XSSFWorkbook();
-		String fileName = _now.toString("jefit_yyyyMMdd_HHmmss.xlsx");
+		Workbook wb = new HSSFWorkbook();
+		String fileName = _now.toString("jefit_yyyyMMdd_HHmmss.xls");
 		File file = new File(context.getExternalFilesDir(null), fileName);
 		FileOutputStream fileOut = new FileOutputStream(file);
 		wb.write(fileOut);
